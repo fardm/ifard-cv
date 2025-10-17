10e3c54948c557d110dfd84bb83f9aa403132d9a
fardm <khotoorat@gmail.com>
2025-10-17 21:17:39 +0330

    remove email & phone & location

--- src/cv.d.ts
+++ src/cv.d.ts
@@ -17,21 +17,12 @@ interface Basics {
   name: string;
   label: string;
   image: string;
-  email: string;
-  phone: string;
   url: string;
   summary: string;
-  location: Location;
   profiles: Array<Profiles>;
 }
 
-interface Location {
-  address: string;
-  postalCode: string;
-  city: string;
-  countryCode: string;
-  region: string;
-}
+
 
 interface Profiles {
   network: string;
